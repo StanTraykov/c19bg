890e80140f382e4d65a0a37c5f2dc4dbee809b8d
Stan Traykov <st@gmuf.com>
2020-12-08 03:42:24 +0200

    Create pubweb.sh

---
+++ pubweb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# publish to web
+git=git
+repo=github.com/StanTraykov/C19_BG
+user=StanTraykov
+pass=$(cat /c/Users/Stan/secret/gitsecret | tr -d "\r\n")
+giturl="https://${user}:${pass}@${repo}"
+rdir="/c/Users/Stan/Documents/webc19/C19_BG"
+copy_dir="${rdir}/cur_svg"
+git_push="${git} push $giturl"
+git_fetch="${git} fetch"
+git_pull="${git} pull"
+git_stage="${git} add ."
+git_commit="${git} commit -m"
+tday=$(date +%b%d)
+yday=$(date -d "${tday}-1day" +%b%d)
+if [[ -d "${tday}" ]]; then
+    src_dir="${tday}"
+elif [[ -d "${yday}" ]]; then
+    src_dir="${yday}"
+else
+    echo "can't find suitable source dir (tried ${tday}, ${yday})" >&2
+    exit 1
+fi
+pub_files="${src_dir}/svg/C*.svg ${src_dir}/svg/D00*.svg ${src_dir}/svg/D*BG.svg ${src_dir}/png/C*heat.png"
+cp -a ${pub_files} "${copy_dir}"
+cd "${copy_dir}" || exit 1
+${git_fetch} && ${git_pull} && ${git_stage} && ${git_commit} "auto upload ${src_dir}"
+${git_push}
