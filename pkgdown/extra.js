8c7a008bc1d44d6a6e8537136bd13f3b0da74a1b
Stan Traykov <st@gmuf.com>
2020-12-31 00:17:32 +0200

    ga fix, DESCRIPTION URLs,

--- pkgdown/extra.js
+++
@@ -1,9 +0,0 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-K3H94J0ECQ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-K3H94J0ECQ');
-</script>
