9e887efd2363c69a9eca835a14a7b9a516a572b2
Stan Traykov <st@gmuf.com>
2021-05-15 21:55:40 +0300

    rm wkd_tab from public src

--- R/opendata_nsi.R
+++ R/opendata_nsi.R
@@ -3,25 +3,25 @@
 #' @importFrom magrittr %>%
 
 process_nsi_data <- function(redownload = FALSE) {
-    wkd_tab <- tib_read_csv2(file = "nsi_wkd.csv",
-                             col_names = FALSE,
-                             col_types = paste0(strrep("c", 162), "_"))
-    names(wkd_tab)[1:3] <- c("year", "oblast", "age")
-    names(wkd_tab)[-(1:3)] <- paste(wkd_tab[1,-(1:3)],
-                                    wkd_tab[2,-(1:3)],
-                                    sep = ":")
-    wkd_tab <- wkd_tab[-(1:2),] %>%
-        dplyr::mutate(
-            age = gsub(paste(" |", tra("nsi_gdot")),"", age),
-            oblast = sub(tra("nsi_sofia_obl"), tra("Sofijska oblast"), oblast),
-            oblast = sub(tra("nsi_sofia_grad"), tra("Sofia-grad"), oblast)
-        ) %>%
-        tidyr::pivot_longer(
-            cols = tidyr::matches(tra("nsi_week")),
-            names_to = c("week", "sex"),
-            names_pattern = paste(tra("nsi_week"), "([0-9]+):(.*)"),
-            values_to = "deaths"
-        )
+    # wkd_tab <- tib_read_csv2(file = "nsi_wkd.csv",
+    #                          col_names = FALSE,
+    #                          col_types = paste0(strrep("c", 162), "_"))
+    # names(wkd_tab)[1:3] <- c("year", "oblast", "age")
+    # names(wkd_tab)[-(1:3)] <- paste(wkd_tab[1,-(1:3)],
+    #                                 wkd_tab[2,-(1:3)],
+    #                                 sep = ":")
+    # wkd_tab <- wkd_tab[-(1:2),] %>%
+    #     dplyr::mutate(
+    #         age = gsub(paste(" |", tra("nsi_gdot")),"", age),
+    #         oblast = sub(tra("nsi_sofia_obl"), tra("Sofijska oblast"), oblast),
+    #         oblast = sub(tra("nsi_sofia_grad"), tra("Sofia-grad"), oblast)
+    #     ) %>%
+    #     tidyr::pivot_longer(
+    #         cols = tidyr::matches(tra("nsi_week")),
+    #         names_to = c("week", "sex"),
+    #         names_pattern = paste(tra("nsi_week"), "([0-9]+):(.*)"),
+    #         values_to = "deaths"
+    #     )
     ##### nsi_data
     nsi_data <- list(
         # from https://www.nsi.bg/bg/content/2977/%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8-%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%D0%B8-%D0%B2%D1%8A%D0%B7%D1%80%D0%B0%D1%81%D1%82-%D0%BC%D0%B5%D1%81%D1%82%D0%BE%D0%B6%D0%B8%D0%B2%D0%B5%D0%B5%D0%BD%D0%B5-%D0%B8-%D0%BF%D0%BE%D0%BB
@@ -35,7 +35,7 @@ process_nsi_data <- function(redownload = FALSE) {
                          106598, 226671, 122546, 1328790, 409265, 313396,
                          119190, 184119, 117335,  252776, 225317, 666801,
                          116915, 158204, 103532,  302694),
-        weekly_deaths = wkd_tab
+        #weekly_deaths = wkd_tab
     )
     return(nsi_data)
 }
