249e776046abe45521ce9ad69dd6f08d0df4b9d1
Stan Traykov <st@gmuf.com>
2021-08-08 21:31:54 +0300

    update to 2020 NSI pop data

--- R/opendata_nsi.R
+++ R/opendata_nsi.R
@@ -24,17 +24,24 @@ process_nsi_data <- function(redownload = FALSE) {
     #     )
     ##### nsi_data
     nsi_data <- list(
+        # 2020 age struct
         # from https://www.nsi.bg/bg/content/2977/%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE-%D1%81%D1%82%D0%B0%D1%82%D0%B8%D1%81%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8-%D1%80%D0%B0%D0%B9%D0%BE%D0%BD%D0%B8-%D0%B2%D1%8A%D0%B7%D1%80%D0%B0%D1%81%D1%82-%D0%BC%D0%B5%D1%81%D1%82%D0%BE%D0%B6%D0%B8%D0%B2%D0%B5%D0%B5%D0%BD%D0%B5-%D0%B8-%D0%BF%D0%BE%D0%BB
-        # age struct 10      0-19,   20-29,   30-39,   40-49,   50-59,
-        #                   60-69,   70-79,   80-89,     90+,   total
-        age_struct_10 = c(1315235,  692250,  956388, 1055350,  953355,
-                           938635,  701964,  301703,   36602, 6951482),
-        # 2019 oblasts pop
-        oblasts_pops = c( 82835, 236305, 171809,  215477, 108018, 172262,
-                         159470, 232568, 127001,  110789, 469885, 110914,
-                         106598, 226671, 122546, 1328790, 409265, 313396,
-                         119190, 184119, 117335,  252776, 225317, 666801,
-                         116915, 158204, 103532,  302694)
+        # age struct 10y  0-19,   20-29,   30-39,   40-49,   50-59,
+        #                 60-69,   70-79,   80-89,     90+,   total
+        age_struct_10 = c(1313252,  670806,  953393, 1050347,  956606,
+                           919675,  716566,  298212,   37691, 6916548),
+        # 2020 oblasts pop
+        # from https://www.nsi.bg/bg/content/2975/%D0%BD%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%BE-%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D0%B8-%D0%BE%D0%B1%D1%89%D0%B8%D0%BD%D0%B8-%D0%BC%D0%B5%D1%81%D1%82%D0%BE%D0%B6%D0%B8%D0%B2%D0%B5%D0%B5%D0%BD%D0%B5-%D0%B8-%D0%BF%D0%BE%D0%BB
+        #                   VID,    PVN,    DOB,     RSE,    SLS,    SHU,
+        #                   VRC,    VTR,    MON,     RAZ,    VAR,    TGV,
+        #                   GAB,    SFO,    LOV,     SOF,    BGS,    SZR,
+        #                   PER,    SLV,    JAM,     PAZ,    HKV,    PDV,
+        #                   KNL,    KRZ,    SML,     BLG
+        oblasts_pops = c( 81212, 233438, 170298,  212729, 106852, 171781,
+                         157637, 229718, 125395,  109810, 470124, 110027,
+                         105788, 238476, 122490, 1308412, 409750, 311400,
+                         120426, 182551, 116486,  251300, 223625, 666398,
+                         116619, 160781, 101887,  301138)
         #weekly_deaths = wkd_tab
     )
     return(nsi_data)
